956dad653b0248391c6e84987e53435e8cdaf2bb
Himanshu Aggarwal <hemanshu0503@gmail.com>
2016-07-23 12:50:22 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,5 +1,6 @@
 echo 'Hello runSh'
-sleep 5m
 echo 'woke up early'
 cat /build/previousOutput/hello.js
 echo "hello this works!!!" > /build/output/hello.js
+echo "works here!!!!"
+echo "works here!!!!"
