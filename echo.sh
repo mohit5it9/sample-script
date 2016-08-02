4323b3bd0b1f42791a0a1688cae4d60d4b36f350
Himanshu Aggarwal <hemanshu0503@gmail.com>
2016-08-02 12:34:04 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,7 +1,8 @@
 echo 'Hello runSh'
 echo 'woke up early'
 echo "hello this works!!!" > /build/output/hello.js
-echo "don't sleep 10 minutes"
+echo "sleep for 5 minutes"
+sleep 5m
 echo "works here!!!!"
 echo "works here!!!!"
 echo "hey niraj"
